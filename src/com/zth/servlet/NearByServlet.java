b205289df7a1f8766ff6253c808979b0865b2e54
zhaotianhua <eezprince@foxmail.com>
2015-03-11 18:08:39 +0800

    first

--- src/com/zth/servlet/NearByServlet.java
+++ src/com/zth/servlet/NearByServlet.java
@@ -2,25 +2,38 @@ package com.zth.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONArray;
+import com.zth.bean.Person;
+
+import net.sf.json.JSONObject;
 
 public class NearByServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3197348907976768047L;
 
 	/**
 	 * The doGet method of the servlet. <br>
 	 *
 	 * This method is called when a form has its tag value method equals to get.
 	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -31,21 +44,29 @@ public class NearByServlet extends HttpServlet {
 	/**
 	 * The doPost method of the servlet. <br>
 	 *
-	 * This method is called when a form has its tag value method equals to post.
+	 * This method is called when a form has its tag value method equals to
+	 * post.
 	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+		List<Person> list = new ArrayList<Person>();
+		list.add(new Person(1, 1.1, 2.2));
+		list.add(new Person(2, 1.1, 2.2));
+		list.add(new Person(3, 1.1, 2.2));
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		//out.write(new Gson);
-		JSONArray jsonArray = new JSONArray();
-		out.print(jsonArray.toString());
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("NearByList", list);
+		out.print(jsonObject);
 		out.flush();
 		out.close();
 	}
